5c4e4f0f05bb06df7218176b6017420064e15d11
elsv <elsv99@gmail.com>
2024-09-26 11:02:00 +0200

    camera script and new assets

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform player;
+    public Vector3 offset = new Vector3(2, 0, -10);
+    public float smoothTime = 0.25f;
+
+    Vector3 currentVelocity;
+
+    private void LateUpdate()
+    {
+        Vector3 target = new Vector3(0, player.position.y, player.position.z) + offset;
+        transform.position = target;
+    }
+
+}
